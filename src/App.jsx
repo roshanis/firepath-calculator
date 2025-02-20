d7825b9df6bf740d6a9a7da2839c77f679309c7e
Roshan Venugopal <roshan.venugopal@gmail.com>
2025-02-19 21:13:51 -0600

    vercel changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Analytics } from "@vercel/analytics/react"
+
+function App() {
+  return (
+    <>
+      {/* ... existing components ... */}
+      <Analytics />
+    </>
+  )
+}
+
+export default App 
